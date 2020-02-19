1a280631458505bac1b75254f14cc1aea704b5e3
chiefguti <sergsguti@gmail.com>
2020-02-19 16:32:43 +0100

    Functional implementacion of ML regions

--- Nonlinear Fiber Channel/RandomForestVisualtisation.py
+++ Nonlinear Fiber Channel/RandomForestVisualtisation.py
@@ -8,16 +8,14 @@ Created on Fri Jan 10 20:00:42 2020
 
 import os
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV , RandomizedSearchCV
-from sklearn.metrics import classification_report
-from scipy import stats
 from sklearn.ensemble import RandomForestClassifier
+from scipy.spatial import Voronoi, voronoi_plot_2d
 import numpy as np 
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
 
 def visualize_classifier(model, X, y,namef, ax=None, cmap='tab20b'):
     ax = ax or plt.gca()
@@ -56,7 +54,7 @@ def NormData(DataSet,data):
         DataSet[i,1]=data[:,1][i]
     DataSet[:,0]=(DataSet[:,0]-min(DataSet[:,0]))/(max(DataSet[:,0])-min(DataSet[:,0]))
     DataSet[:,1]=(DataSet[:,1]-min(DataSet[:,1]))/(max(DataSet[:,1])-min(DataSet[:,1]))
-def ColorMap(Word,Map,M):
+def ColorMap(Word,Org,Map,M):
     Cmap=np.arange(M)
     for i in range(len(Word)):
         for j in range(len(Cmap)):
@@ -70,34 +68,86 @@ def BER(x,y):
             j+=1
     return(j/len(x))
     
+
+
+def VoronoiPlot(data,Map,ConPoint,NameFig):
+    points=[]
+    for i in range(len(data)):
+        points.append([data[:,0][i]/max(data[:,0]),data[:,1][i]/max(data[:,0])])
+    kmeans = KMeans(n_clusters=ConPoint, random_state=0).fit(points)
+    mapK=kmeans.labels_
+    center=kmeans.cluster_centers_
+    Voromap=np.zeros((ConPoint,2))
+    Class=np.arange(0,ConPoint)
+    for i in Class:
+        Voromap[:,0][i]=np.where(Map==i)[0][0]
+        Voromap[:,1][i]=mapK[np.where(Map==i)[0][0]]
+    mtest=np.zeros(len(mapK))
+    for i in range(len(Voromap)):
+        serch=np.where(mapK==Voromap[:,1][i])[0]
+        for j in serch:
+            mtest[j]=i
+    mapK=mtest    
+    vor=Voronoi(center)
+    voronoi_plot_2d(vor, show_vertices=False, line_colors='black',line_width=2, line_alpha=0.6, point_size=2)
+    plt.scatter(data[:,0]/max(data[:,0]),data[:,1]/max(data[:,1]),c=mapK,s=1)
+    plt.grid(True)
+    plt.colorbar()
+    plt.savefig(NameFig)
+    return mapK
+
+def RandForest(DataSet,Map,best,NameFig):
+    X_train, X_test, y_train, y_test = train_test_split(DataSet, Map, test_size = 0.20)
+    gridF = GridSearchCV(RandomForestClassifier(), random_grid, cv = 5, verbose = 1, n_jobs = -1)
+    bestF = gridF.fit(X_train, y_train)
+    best.append(gridF.best_params_)
+    forest=RandomForestClassifier(max_depth=best[i]['max_depth'],max_features=best[i]['max_features'],min_samples_leaf=best[i]['min_samples_leaf'],min_samples_split=best[i]['min_samples_split'],n_estimators=best[i]['n_estimators'])
+    forest.fit(X_train,y_train)
+    y_prediction=forest.predict(X_test)
+    visualize_classifier(forest,X_train,y_train,NameFig)
+    return y_prediction
+
+
+
+def PBRSdecod(Code,bits):
+    INT=[]
+    for i in range(int(len(Code)/bits)):
+        INT.append(i*bits)
+        CodeWord=[]
+    for i in range(len(Code)):
+        CodeWord.append(str(int(Code[i])))
+    Word=[]
+    Org=[]
+    j=0
+    for i in INT:
+        Word.append(''.join(CodeWord[i:i+bits]))
+        if((Word[j] in Org)==False ):
+            Org.append(Word[j])
+        j=j+1
+    return Word , Org
+
+
+
+
 Code = np.loadtxt('PBRS.txt',skiprows=5)
 
+Word, Org=PBRSdecod(Code,4)
 
-INT=[]
-for i in range(int(len(Code)/4)):
-    INT.append(i*4)
-CodeWord=[]
-for i in range(len(Code)):
-    CodeWord.append(str(int(Code[i])))
-
-Word=[]
-Org=[]
-j=0
-for i in INT:
-    Word.append(CodeWord[i]+CodeWord[i+1]+CodeWord[i+2]+CodeWord[i+3])
-    
-    if((Word[j] in Org)==False ):
-        Org.append(Word[j])
-    j=j+1
 Map=np.zeros(len(Word))
-ColorMap(Word,Map,16)
+
+ColorMap(Word,Org,Map,16)
+
+#Map=Map[:len(DataSet)]
+
+
+
 
 # Number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 200, stop = 500, num = 1)]
+n_estimators = [int(x) for x in np.linspace(start = 200, stop = 500, num = 3)]
 # Number of features to consider at every split
 max_features = ['auto', 'sqrt']
 # Maximum number of levels in tree
-max_depth = [int(x) for x in np.linspace(5, 20, num = 1)]
+max_depth = [int(x) for x in np.linspace(5, 20, num = 3)]
 max_depth.append(None)
 # Minimum number of samples required to split a node
 min_samples_split = [2, 5, 10]
@@ -109,19 +159,24 @@ random_grid = {'n_estimators': n_estimators,
                'max_depth': max_depth,
                'min_samples_split': min_samples_split,
                'min_samples_leaf': min_samples_leaf}
-print(random_grid)
+
+
 
 N=np.arange(0,10,1)
-L=np.arange(20,90,10)
-p=[5,10,15,20,25,30,35,40,44,50]
+L=np.arange(20,100,20)
+p=[5,10,15,20,25,30,35,40,44,50,54,60]
 SERforest=np.zeros((len(N),len(L),len(p)))
 SERforestmit=np.zeros((len(N),len(L),len(p)))
 best=[]
 bestmit=[]
-for l in range(len(L)):
+
+
+
+
+for l in range(len(L[:2])):
     for n in range(len(N)):
-        for i in range(len(p)):
-            data = np.loadtxt('N%iL%ikmP%imW.txt'%(N[n],L[l],p[i]))
+        for i in range(len(p[:2])):
+            data = np.loadtxt('ZDF4bQAM%imW%iKm%in.txt'%(p[i],L[l],N[n]))
             datamit=[]
             datamit.append(data[:,2])
             datamit.append(data[:,3])
@@ -130,24 +185,25 @@ for l in range(len(L)):
             NormData(DataSet,data)
             DataSetmit=np.zeros((len(data[:,0]),2))
             NormData(DataSetmit,datamit)
-            plt.scatter(DataSet[:,0],DataSet[:,1],c=Map,s=1,cmap='tab20b')
+            plt.scatter(DataSet[:,0],DataSet[:,1],c=Map[:len(DataSet)],s=1,cmap='tab20b')
             plt.tick_params(axis='both', which='both',length=0)
             plt.savefig('DataN%iL%ikmP%imW.png'%(N[n],L[l],p[i]),dpi=250)
             plt.close()
-            X_train, X_test, y_train, y_test = train_test_split(DataSet, Map, test_size = 0.20)
-            gridF = GridSearchCV(RandomForestClassifier(), random_grid, cv = 2, verbose = 1, n_jobs = -1)
-            bestF = gridF.fit(X_train, y_train)
-            best.append(gridF.best_params_)
-            forest=RandomForestClassifier(max_depth=best[i]['max_depth'],max_features=best[i]['max_features'],min_samples_leaf=best[i]['min_samples_leaf'],min_samples_split=best[i]['min_samples_split'],n_estimators=best[i]['n_estimators'])
-            forest.fit(X_train,y_train)
-            y_prediction=forest.predict(X_test)
-            visualize_classifier(forest,X_train,y_train,'ConstN%iL%ikmP%imW.png'%(N[n],L[l],p[i]))
-            SERforest[n,l,i]=BER(y_test,y_prediction)
-            Xmit_train, Xmit_test, ymit_train, ymit_test = train_test_split(DataSetmit, Map, test_size = 0.20)
-            gridFmit = GridSearchCV(RandomForestClassifier(), random_grid, cv = 2, verbose = 1, n_jobs = -1)
-            bestFmit = gridFmit.fit(Xmit_train, ymit_train)
-            bestmit.append(gridFmit.best_params_)
-            forestmit=RandomForestClassifier(max_depth=bestmit[i]['max_depth'],max_features=bestmit[i]['max_features'],min_samples_leaf=bestmit[i]['min_samples_leaf'],min_samples_split=bestmit[i]['min_samples_split'],n_estimators=bestmit[i]['n_estimators'])
-            forestmit.fit(Xmit_train,ymit_train)
-            ymit_prediction=forestmit.predict(Xmit_test)
-            SERforestmit[n,l,i]=BER(ymit_test,ymit_prediction)
+#            
+#            X_train, X_test, y_train, y_test = train_test_split(DataSet, Map, test_size = 0.20)
+#            gridF = GridSearchCV(RandomForestClassifier(), random_grid, cv = 5, verbose = 1, n_jobs = -1)
+#            bestF = gridF.fit(X_train, y_train)
+#            best.append(gridF.best_params_)
+#            forest=RandomForestClassifier(max_depth=best[i]['max_depth'],max_features=best[i]['max_features'],min_samples_leaf=best[i]['min_samples_leaf'],min_samples_split=best[i]['min_samples_split'],n_estimators=best[i]['n_estimators'])
+#            forest.fit(X_train,y_train)
+#            y_prediction=forest.predict(X_test)
+#            visualize_classifier(forest,X_train,y_train,'ConstN%iL%ikmP%imW.png'%(N[n],L[l],p[i]))
+#            SERforest[n,l,i]=BER(y_test,y_prediction)
+            #Xmit_train, Xmit_test, ymit_train, ymit_test = train_test_split(DataSetmit, Map, test_size = 0.20)
+            #gridFmit = GridSearchCV(RandomForestClassifier(), random_grid, cv = 6, verbose = 1, n_jobs = -1)
+            #bestFmit = gridFmit.fit(Xmit_train, ymit_train)
+            #bestmit.append(gridFmit.best_params_)
+            #forestmit=RandomForestClassifier(max_depth=bestmit[i]['max_depth'],max_features=bestmit[i]['max_features'],min_samples_leaf=bestmit[i]['min_samples_leaf'],min_samples_split=bestmit[i]['min_samples_split'],n_estimators=bestmit[i]['n_estimators'])
+            #forestmit.fit(Xmit_train,ymit_train)
+            #ymit_prediction=forestmit.predict(Xmit_test)
+            #SERforestmit[n,l,i]=BER(ymit_test,ymit_prediction)
